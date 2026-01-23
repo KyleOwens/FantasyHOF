9a45f0ad0f37cff524ac75ea32a034bccca8eac3
KyleOwens4 <owens.w.kyle@gmail.com>
2026-01-23 11:28:00 -0600

    actually build in routing update

--- src/backend/FantasyHOF/wwwroot/assets/dashboard-B-W_x8re.js
+++
@@ -1,1 +0,0 @@
-import{F as e,j as t,aD as a}from"./index-CkkNE_pM.js";function n(){const o=e.useLoaderData();return t.jsx(a,{queryRef:o})}export{n as component};
---
+++ src/backend/FantasyHOF/wwwroot/assets/dashboard-BBN2KlFP.js
@@ -0,0 +1,1 @@
+import{F as e,j as t,aD as a}from"./index-CMLrld1n.js";function n(){const o=e.useLoaderData();return t.jsx(a,{queryRef:o})}export{n as component};
